0ee6c0aeb78a4085228f1b36c225222ef6ed1354
andifa06 <138573377+andifa06@users.noreply.github.com>
2023-07-04 16:34:44 +0700

    Create knn.py

---
+++ knn.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import requests
+
+# Load dataset from Github
+url = "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv"
+res = requests.get(url)
+df = pd.read_csv(url)
+
+# Split dataset into features and target variable
+X = df.drop('species', axis=1)
+y = df['species']
+
+# Split dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train K-NN classifier
+clf = KNeighborsClassifier(n_neighbors=5)
+clf.fit(X_train, y_train)
+
+# Predict the target variable for test dataset
+y_pred = clf.predict(X_test)
+
+# Evaluate the performance of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
